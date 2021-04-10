9fd64c9184aeb361dd3a1e940e86c451ff562fbc
Samuel Swank <samswank@gmail.com>
2021-04-09 22:33:03 -0600

    one-liner directional control

---
+++ local/python/game.py
@@ -0,0 +1,41 @@
+from charachter import Player
+from item import Item
+from room import Room
+
+rooms = {
+    "dungeon" : Room(
+        "Dungeon",
+         "Ye find yeself in yon dungeon. Ye see a SCROLL. Behind ye SCROLL is a FLASK. Obvious exits are NORTH, SOUTH, and DENNIS"
+        ),
+    "parapets" : Room(
+        "Parapets",
+        "You arrive at parapets. Ye see a rope. Obvious exits are SOUTH."
+        ),
+    "embankment" : Room(
+        "Embankment",
+        "Or maybe a chasm. You can't decide which. Anyway, ye spies a TRINKET. Obvious exits are NORTH."
+    ),
+    "Dennis" : Room(
+        "Dennis",
+        "Ye arrive at Dennis. He wears a sporty frock coat and a long jimber jam. He paces about nervously. Obvious exits are NOT DENNIS."
+    )
+}
+
+# Link rooms together
+rooms["dungeon"].n_to = rooms["parapets"]
+rooms["dungeon"].s_to = rooms["embankment"]
+rooms["dungeon"].dennis_to = rooms["Dennis"]
+rooms["parapets"].s_to = rooms["dungeon"]
+rooms["embankment"].n_to = rooms["dungeon"]
+rooms["Dennis"].not_dennis_to = rooms["dungeon"]
+
+player = Player(current_room=rooms["dungeon"])
+print(player.current_room.description)
+
+while True:
+
+    input_ = input("What wouldst thou deau?")
+
+    if player.current_room.__getattribute__(input_):
+        player.__getattribute__(input_)
+        print(player.current_room.description)
