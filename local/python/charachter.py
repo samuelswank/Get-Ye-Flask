e410b6a4196933dcf70414f0ac6b3e936c018ee6
Samuel Swank <samswank@gmail.com>
2021-04-09 22:31:54 -0600

    simplifed charachter attributes

--- local/python/charachter.py
+++ local/python/charachter.py
@@ -1,15 +1,13 @@
 from room import Room
 
 class Player:
-    def __init__(self, name, sex, current_room, inventory, score):
-        self.name = name
-        self.sex = sex
+    def __init__(self, current_room, inventory=[], score=0):
         self.current_room = current_room
-        self.inventory = []
-        self.score = 0
+        self.inventory = inventory
+        self.score = score
 
     def get_item(self, item):
         self.current_room.items.remove(item)
         self.inventory.append(item)
 
-        
+    
