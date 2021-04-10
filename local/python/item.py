f293027efaaafae07a255fec1ddde3cd901fa805
Samuel Swank <samswank@gmail.com>
2021-04-10 13:43:02 -0600

    __str__ for item deprecated

--- local/python/item.py
+++ local/python/item.py
@@ -4,6 +4,3 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    def __str__(self):
-        return f"name: {self.name}, description: {self.description}"
