6039149d5b837940b6d4403707b891cda45aec8b
Samuel Swank <samswank@gmail.com>
2021-04-09 22:32:32 -0600

    clarified directional attributes

--- local/python/room.py
+++ local/python/room.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None, dennis_to=None):
+    def __init__(self, name, description, n_to=None, s_to=None, dennis_to=None, not_dennis_to=None):
         self.name = name
         self.description = description
 
